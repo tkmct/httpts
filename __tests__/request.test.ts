8c357d6b0254f20c4a2a5da69d12b75fe8c0bd24
tkmct <1220t.takamichi@gmail.com>
2018-12-03 15:39:43 +0900

    Add test

--- __tests__/request.test.ts
+++ __tests__/request.test.ts
@@ -1,6 +1,6 @@
 import { parse } from '../src/request'
 
-test('request parser', () => {
+test('parse header', () => {
   const req =
     'GET / HTTP/1.1\r\nHost: localhost:8080\r\nUser-Agent: curl/7.54.0\r\nAccept: */*\r\n\r\n'
 
@@ -8,4 +8,6 @@ test('request parser', () => {
 
   expect(request.header.Method).toBe('GET')
   expect(request.header.Host).toBe('localhost:8080')
+  expect(request.header['User-Agent']).toBe('curl/7.54.0')
+  expect(request.header.Accept).toBe('*/*')
 })
