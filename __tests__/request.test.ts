fd0fe07fc563f0ab0c789b649158dce3ad34b134
tkmct <1220t.takamichi@gmail.com>
2018-12-04 17:26:34 +0900

    Add post test

--- __tests__/request.test.ts
+++ __tests__/request.test.ts
@@ -1,12 +1,19 @@
 import { parse, parseHeader } from '../src/request'
 
 describe('Parse Request', () => {
-  test('parse', () => {
+  test('parse get request', () => {
     const req =
       'GET / HTTP/1.1\r\nHost: localhost:8080\r\nUser-Agent: curl/7.54.0\r\nAccept: */*\r\n\r\n'
 
     const request = parse(req)
     expect(request.body).toBeNull()
+  })
+
+  test('parse post request', () => {
+    const req =
+      'POST / HTTP/1.1\r\nHost: localhost:8080\r\nUser-Agent: curl/7.54.0\r\nAccept: */*\r\nContent-Length: 27\r\nContent-Type: application/x-www-form-urlencoded\r\n\r\nparam1=value1&param2=value2'
+    const request = parse(req)
+    expect(request.body).toBe('param1=value1&param2=value2')
   })
 
   test('parseHeader valid', () => {
