e6136ed12c529cc4f94ba53a3fb5d0bb029cfa80
tkmct <1220t.takamichi@gmail.com>
2018-12-04 17:26:59 +0900

    parse request in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 import * as net from 'net'
+import { parse } from './request'
 
 function main() {
   const server = net.createServer(socket => {
     socket.on('data', buffer => {
-      console.info(JSON.stringify(buffer.toString()))
+      const r = buffer.toString()
+      const request = parse(r)
+      console.log(request)
 
       socket.write('Request sent')
       socket.end()
